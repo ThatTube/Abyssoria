49f9035941adb6cad5273bdf7f9bc3263da52f91
Lu <164914586+ThatTube@users.noreply.github.com>
2026-05-06 22:25:29 -0300

    ss

---
+++ src/main/java/net/mcreator/abyssoria/item/JunioitrItem.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.abyssoria.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class JunioitrItem extends Item {
+	public JunioitrItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
